3da4faea60a2b9505870f73599e0dee0daf71275
rparagas <rayparagas@rays-air.home>
2016-12-09 14:02:56 +1100

    added navigation controller, included add comic button, added camera and camera roll navigation buttons, access camera roll functionality included with privacy request

---
+++ ComicViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ComicViewController.swift
+//  Camera-ComicCollector
+//
+//  Created by Ray Paragas on 9/12/16.
+//  Copyright © 2016 Ray Paragas. All rights reserved.
+//
+
+import UIKit
+
+class ComicViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var comicImageView: UIImageView!
+    @IBOutlet weak var comicTextField: UITextField!
+    
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imagePicker.delegate = self
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        comicImageView.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func photosTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+
+}
